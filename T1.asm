733ef26fbae70432f7766da5c6ac57c090a01c1a
Malaika Zaim <k253108@nu.edu.pk>
2026-09-05 23:26:06 +0500

    t1 updated

--- T1.asm
+++ T1.asm
@@ -4,19 +4,18 @@ a DWORD 10h
 b DWORD 12h
 c1 DWORD 30h
 d DWORD 20h
-
 .code
 main PROC
 	mov eax, a
 	add eax, b
 
-	mov ebx, a
-	sub ebx, b
-
-	sub eax, ebx
+	mov ecx, a
+	sub ecx, b
+	sub eax, ecx
 
 	add eax, c1
 	add eax, d
+	mov ebx, eax
 	call Dumpregs
 	exit
 main ENDP
